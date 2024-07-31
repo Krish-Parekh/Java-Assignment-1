f640cb924ae4e366ccc252c89221197fcd4ce64d
Krish-Parekh <krishparekh303@gmail.com>
2024-07-31 09:49:25 +0530

    feat: changes into ListAllBook class

--- operations/ListAllBook.java
+++ operations/ListAllBook.java
@@ -1,18 +1,17 @@
 
-
 import java.util.ArrayList;
 
 import Library.interfaces.Operation;
 
 public class ListAllBook implements Operation {
     private ArrayList<Book> books;
+
     public ListAllBook(ArrayList<Book> books) {
         this.books = books;
     }
-    
+
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
         if (books.isEmpty()) {
             System.out.println("No books available");
         } else {
@@ -21,6 +20,6 @@ public class ListAllBook implements Operation {
                 int bookNumber = i + 1;
                 System.out.println(bookNumber + ". " + books.get(i).toString());
             }
-        }   
+        }
     }
 }
