e99766671ffba34f7ab1ada83c3a22b14d11da42
Krish-Parekh <krishparekh303@gmail.com>
2024-08-02 04:55:41 +0530

    feat: book adding functionality

---
+++ CheckoutType.java
@@ -0,0 +1,5 @@
+package Library;
+
+public enum CheckoutType {
+    PHYSICAL, EBOOK;
+}
--- OperationFactory.java
+++ OperationFactory.java
@@ -43,30 +43,63 @@ public class OperationFactory {
 
     private Book selectBook() {
         Book selectedBook;
-        System.out.println("Enter a keyword: ");
-        String keyword = System.console().readLine();
+        System.out.print("Enter a keyword: ");
+        String keyword = System.console().readLine().toLowerCase();
+
         ArrayList<Book> searchedBooks = new ArrayList<>();
         System.out.println("The following book(s) are found: ");
 
+        // Search for books that contain the keyword
         for (int i = 0; i < allBooks.size(); i++) {
             Book book = allBooks.get(i);
-            if (book.getTitle().contains(keyword)) {
+            if (book.getTitle().toLowerCase().contains(keyword)) {
                 searchedBooks.add(book);
             }
         }
 
+        // Display the search results
         int bookNumber = 0;
         for (int i = 0; i < searchedBooks.size(); i++) {
             ++bookNumber;
             System.out.println(bookNumber + ". " + searchedBooks.get(i).getTitle());
         }
-        System.out.println((bookNumber + 1) + ". Cancel");
 
+        // Display the cancel option
+        if (bookNumber == 0) {
+            System.out.println("No book found.");
+            return null;
+        } else {
+            System.out.println((bookNumber + 1) + ". Cancel");
+        }
+
+        // Select a book number to add to the cart
+        System.out.print("Please select: ");
         int selectedBookNumber = Integer.parseInt(System.console().readLine());
         if (selectedBookNumber == bookNumber + 1) {
             return null;
         }
+
+        // Ask the user, if they want to buy it as a EBook
         selectedBook = searchedBooks.get(selectedBookNumber - 1);
+
+        System.out.print("Do you want to buy this as an ebook: ");
+        String buyAsEBook = System.console().readLine();
+        if (buyAsEBook.equals("yes")) {
+            if (selectedBook.getHasEBook()) {
+                System.out.println("EBook bought successfully");
+            } else {
+                System.out.println("EBook not available");
+                return null;
+            }
+        } else {
+            if (selectedBook.getNCopies() > 0) {
+                selectedBook.setNCopies(selectedBook.getNCopies() - 1);
+                System.out.println("Book bought successfully");
+            } else {
+                System.out.println("Book not available");
+                return null;
+            }
+        }
 
         return selectedBook;
     }
