82abd0a3ccc1fbb713b0c96f9a1225529a7423eb
Krish-Parekh <krishparekh303@gmail.com>
2024-08-17 09:45:02 +0530

    fix: view book copy type operation

--- Operation/ViewBookOperation.java
+++ Operation/ViewBookOperation.java
@@ -25,7 +25,8 @@ public class ViewBookOperation implements Operation {
             LinkedHashMap<BookType, ArrayList<CartBook>> bookTypeLookup = bookLookup.get(bookName);
             for (BookType bookType : bookTypeLookup.keySet()) {
                 ArrayList<CartBook> cartBooks = bookTypeLookup.get(bookType);
-                System.out.println(index + ". " + bookName + " | " + cartBooks.size() + " | " + bookType);
+                String copyType = cartBooks.size() > 1 ? "copies" : "copy";
+                System.out.println(index + ". " + bookName + " | " + cartBooks.size() + " " + copyType + " " + " | " + bookType);
                 index++;
             }
         }
